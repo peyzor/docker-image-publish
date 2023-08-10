24077a4bd0523b9eb462db7145d47752d53ecefa
Peyman <hassani.peyman627@gmail.com>
2023-08-10 10:53:15 +0330

    Add simple endpoint in gin

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ WORKDIR /app
 COPY go.mod .
 COPY main.go .
 
+RUN go get
 RUN go build -o bin .
 
 ENTRYPOINT [ "/app/bin" ]
--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
 )
 
 func main() {
-	log.Println("Hello Docker!")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := r.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
